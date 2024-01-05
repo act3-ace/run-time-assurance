fd39797b300f4b5b6318012985d53cd2959c05ad
Charles Keating <Charles.Keating@udri.udayton.edu>
2024-01-05 11:53:28 -0500

    chore(cicd): removed requirements.dep.txt from release assets

--- release.sh
+++ release.sh
@@ -4,7 +4,6 @@ NEXT_RELEASE="$1"
 # Update Poetry version if lock file exist
 if [ -f poetry.lock ]; then
     poetry version "${NEXT_RELEASE}"
-    poetry export -f requirements.txt -o requirements.dep.txt --with lint,test,docs
 fi
 
 # Update Version file
